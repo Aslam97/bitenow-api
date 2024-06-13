31b03454e65d9060d6e584a6f61a2c6f53d6e485
Aslam H <iupin5212@gmail.com>
2024-06-13 10:37:36 +0700

    feat: add day_name and format open and close

--- app/Models/OpeningHour.php
+++ app/Models/OpeningHour.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -16,12 +17,37 @@ class OpeningHour extends Model
         'close',
     ];
 
-    protected function casts(): array
+    protected $appends = [
+        'day_name',
+    ];
+
+    protected function open(): Attribute
     {
-        return [
-            'open' => 'datetime:H:i',
-            'close' => 'datetime:H:i',
+        return Attribute::make(
+            get: fn (mixed $value, array $attributes) => now()->setTimeFromTimeString($attributes['open'])->format('H:i'),
+        );
+    }
+
+    protected function close(): Attribute
+    {
+        return Attribute::make(
+            get: fn (mixed $value, array $attributes) => now()->setTimeFromTimeString($attributes['close'])->format('H:i'),
+        );
+    }
+
+    protected function getDayNameAttribute()
+    {
+        $days = [
+            1 => 'Monday',
+            2 => 'Tuesday',
+            3 => 'Wednesday',
+            4 => 'Thursday',
+            5 => 'Friday',
+            6 => 'Saturday',
+            7 => 'Sunday',
         ];
+
+        return $days[$this->day];
     }
 
     public function business()
